0a44ee342b3838882d1b8f8f3b00da8dc56b297e
Pr75502 <pp1614582@gmail.com>
2025-02-19 12:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function insert_Row() {
     //Write your code here
+	let table=document.querySelector("#simpleTable")
+	let button=document.querySelector("button")
+	button.addEventListener("click", (e)=>{
+		let tr=document.createElement("tr")
+		let td1=document.createElement("td")
+		td1.innerText="New Cell1"
+		let td2=document.createElement("td")
+		td2.innerText="New Cell2"
+		tr.append(td1, td2)
+		table.prepend(tr)
+	
+		
+	})
   
   
 }
