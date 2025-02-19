87b0b53012bb6852f79e9533d9d9926be95fe5dd
Pr75502 <pp1614582@gmail.com>
2025-02-19 13:00:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
+document.querySelector("input").addEventListener(click, insert_Row);
 function insert_Row() {
     //Write your code here
 	let table=document.querySelector("#sampleTable")
-	let button=document.querySelector("button")
-	button.addEventListener("click", ()=>{
+	
+	
 		let tr=document.createElement("tr")
 		let td1=document.createElement("td")
 		td1.innerText="New Cell1"
@@ -10,9 +12,4 @@ function insert_Row() {
 		td2.innerText="New Cell2"
 		tr.append(td1, td2)
 		table.prepend(tr)
-	
-		
-	})
-  
-  
 }
